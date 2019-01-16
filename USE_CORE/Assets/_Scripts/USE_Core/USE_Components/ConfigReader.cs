e0e1d7c708ca62052c6a5561517c8b767b7864b6
Marcus Watson <watsonmr@yorku.ca>
2019-01-16 10:59:45 -0500

    Config reader edits

---
+++ USE_CORE/Assets/_Scripts/USE_Core/USE_Components/ConfigReader.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Reflection;
+using System.ComponentModel;
+using UnityEngine.SceneManagement;
+using ConfigParsing;
+
+public class ConfigReader : MonoBehaviour{
+	//public static ConfigReader instance;  
+
+	private static Dictionary<string,Settings> allConfigs = new Dictionary<string, Settings>();
+
+	public static Settings Get(string target){
+		if (!allConfigs.ContainsKey(target)){
+			Debug.Log("config not found: " + target);
+			return new Settings();
+		}else{
+			return allConfigs[target];
+		}
+	}
+
+	public static void ReadConfig(string key, string fullPath){
+		Settings tmp = ConfigParsing.ConfigParsing.parseConfigFile(fullPath);
+		allConfigs.Add(key,tmp);
+		Debug.Log("read in"+key + ", from: "+fullPath);
+	}
+
+	public static void ListAllConfigs(){
+		foreach (KeyValuePair<string,Settings> kv in allConfigs){
+			Debug.Log(kv.Key + ", warning: " + kv.Value.warning);
+		}
+	}
+}

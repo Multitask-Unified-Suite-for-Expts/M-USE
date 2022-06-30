e15992c9b848a0e4e41f31a7e8199e9461749b22
seemadhungana <seema.dhungana@vanderbilt.edu>
2022-06-30 10:09:33 -0500

    Gaze tracker

---
+++ USE_CORE/Assets/_Scripts/USE_Core/WorkingFolder/InputTrackers/GazeTracker.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using USE_Data;
+
+public class GazeTracker : InputTracker
+{
+    private Vector3 GazePos = new Vector3 (-1f, -1f, -1f);
+    private bool SpoofGazeWithMouse = false;
+    private GameObject HoverObject;
+
+    public override void AddFieldsToFrameData(DataController frameData)
+    {
+        frameData.AddDatum("GazePosition", () => GazePos);
+        frameData.AddDatum("GazePosition1", () => GazePos.x);
+        frameData.AddDatum("GazePosition2", () => GazePos.y);
+        frameData.AddDatum("GazePosition3", () => GazePos.z);
+        frameData.AddDatum("HoverObject", () => HoverObject != null ? HoverObject.name : null);
+    }
+
+    public override GameObject FindCurrentTarget()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(GazePos), out RaycastHit hit, Mathf.Infinity))
+        {
+            HoverObject = hit.transform.root.gameObject;
+            return HoverObject;
+        }
+        return null;
+    }
+    public void CustomUpdate()
+    {
+        if (SpoofGazeWithMouse)
+        {
+            GazePos = InputBroker.mousePosition;
+        }
+    }
+}

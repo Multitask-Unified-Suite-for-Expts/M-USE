d60993dfc3d8a5684b15a062f0e43b6268600e3d
DESKTOP-ICRE39D\WomLab_Unity <seema.dhungana@vanderbilt.edu>
2022-12-15 12:28:54 -0600

    004 commit small changes

--- USE_CORE/Assets/_USE_Tasks/ContinuousRecognition/ContinuousRecognition_TrialLevel.cs
+++ USE_CORE/Assets/_USE_Tasks/ContinuousRecognition/ContinuousRecognition_TrialLevel.cs
@@ -493,20 +493,35 @@ public class ContinuousRecognition_TrialLevel : ControlLevel_Trial_Template
         switch (NumFeedbackRows)
         {
             case 1:
-                    yOffset = 55f; //good
-                    break;
+                if (MacMainDisplayBuild)
+                    yOffset = 85f;
+                else
+                    yOffset = 55f;
+                break;
             case 2:
-                    yOffset = 40f; //good
-                    break;
+                if (MacMainDisplayBuild)
+                    yOffset = 60f;
+                else
+                    yOffset = 40f;
+                break;
             case 3:
-                    yOffset = -5f; //good
-                    break;
+                if (MacMainDisplayBuild)
+                    yOffset = 15f;
+                else
+                    yOffset = -5f;
+                break;
             case 4:
-                    yOffset = -20f; //good
-                    break;
+                if (MacMainDisplayBuild)
+                    yOffset = -25f; //check this!!
+                else
+                    yOffset = -20f; //check
+                break;
             case 5:
-                    yOffset = -30f;
-                    break;
+                if (MacMainDisplayBuild)
+                    yOffset = -30f; //check
+                else
+                    yOffset = -25f; //check
+                break;
         }
         return yOffset;
     }

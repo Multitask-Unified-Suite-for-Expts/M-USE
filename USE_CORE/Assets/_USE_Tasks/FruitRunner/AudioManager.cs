e3bf8cf2e4e1e92833274c93648b90e98d6074ce
Nathan Traczewski <nathantraczewski@gmail.com>
2023-12-11 18:43:59 -0600

    Fruit Runner - Multiple Audio Sources for Different gameobjects
    Added multiple audio sources to audioManager class for Fruit Runner.

--- USE_CORE/Assets/_USE_Tasks/FruitRunner/AudioManager.cs
+++ USE_CORE/Assets/_USE_Tasks/FruitRunner/AudioManager.cs
@@ -5,30 +5,42 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    private AudioSource AudioSource;
+    private AudioSource ItemAudioSource;
+    private AudioSource PlayerAudioSource;
+
     private AudioClip PositiveCollected_AudioClip;
     private AudioClip NegativeCollected_AudioClip;
-
+    private AudioClip Slide_AudioClip;
 
     private void Start()
     {
-        AudioSource = gameObject.GetComponent<AudioSource>();
+        ItemAudioSource = gameObject.AddComponent<AudioSource>();
+        PlayerAudioSource = gameObject.AddComponent<AudioSource>();
         PositiveCollected_AudioClip = Resources.Load<AudioClip>("AudioClips/CorrectItemPickup");
         NegativeCollected_AudioClip = Resources.Load<AudioClip>("AudioClips/WrongItemPickup");
+        Slide_AudioClip = Resources.Load<AudioClip>("AudioClips/Slide");
     }
 
     public void PlayPositiveItemCollected()
     {
-        AudioSource.clip = PositiveCollected_AudioClip;
-        AudioSource.volume = 1f;
-        AudioSource.Play();
+        ItemAudioSource.clip = PositiveCollected_AudioClip;
+        ItemAudioSource.volume = 1f;
+        ItemAudioSource.Play();
     }
 
     public void PlayNegativeItemCollected()
     {
-        AudioSource.clip = NegativeCollected_AudioClip;
-        AudioSource.volume = .2f;
-        AudioSource.Play();
+        ItemAudioSource.clip = NegativeCollected_AudioClip;
+        ItemAudioSource.volume = .2f;
+        ItemAudioSource.Play();
     }
+
+    public void PlaySlideClip()
+    {
+        PlayerAudioSource.clip = Slide_AudioClip;
+        PlayerAudioSource.volume = .2f;
+        PlayerAudioSource.Play();
+    }
+
 
 }

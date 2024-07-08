78e8792b5be9e1dd5dc363d2821ef4bfcad17755
Nathan Traczewski <nathantraczewski@gmail.com>
2024-07-08 08:42:11 -0500

    SA to Keep Track, and AS Tokenbar changes
    Changed all files from Sustained Attention to Keep Track and implemented the new Tokenbar changes needed for AntiSaccade.

--- USE_CORE/Assets/_USE_Tasks/AntiSaccade/AntiSaccade_TrialLevel.cs
+++ USE_CORE/Assets/_USE_Tasks/AntiSaccade/AntiSaccade_TrialLevel.cs
@@ -120,12 +120,10 @@ public class AntiSaccade_TrialLevel : ControlLevel_Trial_Template
             TokenFBController.enabled = false;
             LoadConfigUIVariables();
             TokenFBController.SetTotalTokensNum(CurrentTrial.TokenBarCapacity);
-            TokenFBController.SetTokenBarValue(CurrentTrial.NumInitialTokens);
 
             SetDataStrings();
             
             CreateIcons();
-
 
             //***** SET TARGET STIM *****
             TargetStim_GO = targetStim.stimDefs[0].StimGameObject;
@@ -351,6 +349,10 @@ public class AntiSaccade_TrialLevel : ControlLevel_Trial_Template
         Session.SyncBoxController?.SendRewardPulses(CurrentTrial.NumPulses, CurrentTrial.PulseSize);
         CurrentTaskLevel.NumRewardPulses_InBlock += CurrentTrial.NumPulses;
         CurrentTaskLevel.NumRewardPulses_InTask += CurrentTrial.NumPulses;
+
+        //Reset tokenbar to numInitialTokens
+        TokenFBController.SetTokenBarValue(CurrentTrial.NumInitialTokens);
+
     }
 
     private void MakeStimFaceCamera(StimGroup stims)
@@ -451,6 +453,8 @@ public class AntiSaccade_TrialLevel : ControlLevel_Trial_Template
 
     public void ResetBlockVariables()
     {
+
+
         TrialsCorrect_Block = 0;
         TrialCompletions_Block = 0;
         TokenBarCompletions_Block = 0;
